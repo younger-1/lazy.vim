7a629e352fb2f0ce662952ebcbbef2bb264a9699
Xavier Young <45989017+younger-1@users.noreply.github.com>
2024-10-13 14:42:46 +0800

    init: lazy
    Change-Id: I752a9259ad6e9da33d65ea7937d034a10a2ba7c3

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
+if vim.loader then
+  vim.loader.enable()
+end
+
+vim.env.LAZYVIM_DIR = vim.fn.stdpath("data") .. "/lazy/lazyvim"
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
