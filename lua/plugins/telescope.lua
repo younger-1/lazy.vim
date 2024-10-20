1a6e3e3dd63b3626dba780624237acde51df57b6
Xavier Young <45989017+younger-1@users.noreply.github.com>
2024-10-20 17:22:35 +0800

    new: telescope-lazy
    Change-Id: Id0b071acdc3ea0baaa0030f7912941ac02168d3a

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,16 +2,21 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     opts = function(_, opts)
+      opts.defaults.cache_picker = {
+        num_pickers = 20,
+      }
       return opts
     end,
     keys = {
       {
         "<leader>s<space>",
         "<cmd>Telescope resume<cr>",
+        desc = "Resume",
       },
       {
         "<leader>su",
         "<cmd>Telescope pickers<cr>",
+        desc = "Pickers",
       },
       {
         "<leader>yc",
@@ -106,5 +111,42 @@ return {
         end,
       },
     },
+  },
+  {
+    "tsakirist/telescope-lazy.nvim",
+    keys = {
+      {
+        "<leader>sx",
+        "<cmd>Telescope lazy<cr>",
+        desc = "Lazy plugins",
+      },
+    },
+    dependencies = {
+      {
+        "nvim-telescope/telescope.nvim",
+        opts = {
+          extensions = {
+            lazy = {
+              theme = "ivy",
+              show_icon = true,
+              mappings = {
+                open_in_browser = "<C-o>",
+                open_in_find_files = "<C-f>",
+                open_in_live_grep = "<C-g>",
+                open_in_file_browser = "<C-b>",
+                open_in_terminal = "<C-t>",
+                open_plugins_picker = "<C-o>", -- Works only after having called first another action
+                open_lazy_root_find_files = "<C-r>f",
+                open_lazy_root_live_grep = "<C-r>g",
+                change_cwd_to_plugin = "<C-r>d",
+              },
+            },
+          },
+        },
+      },
+    },
+    config = function()
+      require("telescope").load_extension("lazy")
+    end,
   },
 }
