748b4b7b1df8b9a47e48e71a3633862a5b6ebafc
Xavier Young <45989017+younger-1@users.noreply.github.com>
2024-10-27 19:17:50 +0800

    lint: disable markdownlint-cli2
    Change-Id: I18d6239eb425ec553105f73d5ac7383df5f9eb42

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,6 +3,8 @@
 -- - https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- - $LAZYVIM_DIR/lua/lazyvim/config/options.lua
 
+vim.g.autoformat = false
+
 local o = vim.opt
 
 o.clipboard = ""
