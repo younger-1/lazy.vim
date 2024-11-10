d022a086c9ffdaab143623d5ce3cd81471a178f3
Xavier Young <45989017+younger-1@users.noreply.github.com>
2024-11-10 09:38:15 +0800

    new: treesitter-context
    Change-Id: I783f1c65c17bb4250505e0e9f82c828c99810b97

--- lua/plugins/matchup.lua
+++ lua/plugins/matchup.lua
@@ -1,13 +1,29 @@
 return {
-  "andymass/vim-matchup",
-  event = { "BufReadPost", "BufNewFile" },
-  keys = {
-    { "<C-y>", "<cmd>MatchupWhereAmI?<cr>" },
-  },
-  init = function()
-    vim.g.matchup_matchparen_offscreen = { method = "popup" }
+  {
+    "andymass/vim-matchup",
+    event = { "BufReadPost", "BufNewFile" },
+    keys = {
+      { "<C-y>", "<cmd>MatchupWhereAmI?<cr>" },
+    },
+    init = function()
+      vim.g.matchup_matchparen_offscreen = {
+        -- method = "popup", -- disabled, as we have treesitter-context
+      }
 
-    vim.g.matchup_matchparen_deferred = 1
-    vim.g.matchup_matchparen_hi_surround_always = 1
-  end,
+      vim.g.matchup_matchparen_deferred = 1
+      vim.g.matchup_matchparen_hi_surround_always = 1
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = {
+      matchup = {
+        enable = true, -- mandatory, false will disable the whole extension
+        enable_quotes = true, -- https://github.com/andymass/vim-matchup/issues/287
+        disable_virtual_text = true, -- do not use virtual text to highlight the virtual end of a block, for languages without explicit end markers (e.g., Python).
+        include_match_words = true, -- additionally include traditional vim regex matches for symbols. For example, highlights /* */ comments in C++ which are not supported in tree-sitter matching.
+      },
+    },
+  },
 }
