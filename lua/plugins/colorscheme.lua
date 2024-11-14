5707fc79b8b5042083426aaeee124ff1d85a253e
Xavier Young <45989017+younger-1@users.noreply.github.com>
2024-11-14 17:17:27 +0800

    colorscheme: dim inactive window
    Change-Id: I8f26de86240b6e4c34e6269d814ccac2043a4d27

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,6 +7,22 @@ return {
     },
   },
   {
+    "folke/tokyonight.nvim",
+    ---@module 'tokyonight'
+    ---@type tokyonight.Config
+    opts = {
+      -- moon storm night day
+      style = "moon",
+      dim_inactive = true,
+    },
+  },
+  {
+    "catppuccin/nvim",
+    opts = {
+      dim_inactive = { enabled = true, percentage = 0.8 },
+    },
+  },
+  {
     "bluz71/vim-moonfly-colors",
     name = "moonfly",
     lazy = false,
