8bf624f52b4cd517276b7dc1b72e919a500e0450
Xavier Young <45989017+younger-1@users.noreply.github.com>
2024-12-13 22:29:23 +0800

    cmp-cmdline: lazy load event
    Change-Id: I18cecdbb514832ec4f343eca249d063bd37f62df

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,39 +1,41 @@
+---@type LazySpec
 return {
   {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-cmdline",
+    event = "CmdlineEnter",
+    specs = {
+      "hrsh7th/nvim-cmp",
+      opts = function()
+        LazyVim.on_load("nvim-cmp", function()
+          local cmp = require("cmp")
+          local cmdline_map = cmp.mapping.preset.cmdline({
+            -- ['<C-n>'] = {
+            --   c = function(fallback) fallback() end,
+            -- },
+            -- ['<C-p>'] = {
+            --   c = function(fallback) fallback() end,
+            -- },
+            -- ["<C-e>"] = vim.NIL,
+          })
+
+          -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+          cmp.setup.cmdline(":", {
+            mapping = cmdline_map,
+            sources = {
+              { name = "path" },
+              { name = "cmdline" },
+            },
+          })
+
+          -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+          cmp.setup.cmdline({ "/", "?" }, {
+            mapping = cmdline_map,
+            sources = {
+              { name = "buffer" },
+            },
+          })
+        end)
+      end,
     },
-    opts = function()
-      LazyVim.on_load("nvim-cmp", function()
-        local cmp = require("cmp")
-        local cmdline_map = cmp.mapping.preset.cmdline({
-          -- ['<C-n>'] = {
-          --   c = function(fallback) fallback() end,
-          -- },
-          -- ['<C-p>'] = {
-          --   c = function(fallback) fallback() end,
-          -- },
-          -- ["<C-e>"] = vim.NIL,
-        })
-
-        -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-        cmp.setup.cmdline(":", {
-          mapping = cmdline_map,
-          sources = {
-            { name = "path" },
-            { name = "cmdline" },
-          },
-        })
-
-        -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-        cmp.setup.cmdline({ "/", "?" }, {
-          mapping = cmdline_map,
-          sources = {
-            { name = "buffer" },
-          },
-        })
-      end)
-    end,
   },
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,4 @@
+---@type LazySpec
 return {
   {
     "nvim-telescope/telescope.nvim",
@@ -724,29 +725,27 @@ return {
         cond = false,
         submodules = false,
         specs = {
-          {
-            "nvim-telescope/telescope.nvim",
-            opts = {
-              extensions = {
-                ["zf-native"] = {
-                  -- options for sorting file-like items
-                  file = {
-                    -- override default telescope file sorter
-                    enable = true,
-                    -- highlight matching text in results
-                    highlight_results = true,
-                    -- enable zf filename match priority
-                    match_filename = true,
-                  },
-                  -- options for sorting all other items
-                  generic = {
-                    -- override default telescope generic item sorter
-                    enable = false,
-                    -- highlight matching text in results
-                    highlight_results = true,
-                    -- disable zf filename match priority
-                    match_filename = false,
-                  },
+          "nvim-telescope/telescope.nvim",
+          opts = {
+            extensions = {
+              ["zf-native"] = {
+                -- options for sorting file-like items
+                file = {
+                  -- override default telescope file sorter
+                  enable = true,
+                  -- highlight matching text in results
+                  highlight_results = true,
+                  -- enable zf filename match priority
+                  match_filename = true,
+                },
+                -- options for sorting all other items
+                generic = {
+                  -- override default telescope generic item sorter
+                  enable = false,
+                  -- highlight matching text in results
+                  highlight_results = true,
+                  -- disable zf filename match priority
+                  match_filename = false,
                 },
               },
             },
@@ -765,14 +764,12 @@ return {
           { "kkharji/sqlite.lua" },
         },
         specs = {
-          {
-            "nvim-telescope/telescope.nvim",
-            opts = {
-              defaults = {
-                history = {
-                  path = vim.fs.joinpath(vim.fn.stdpath("data"), "telescope_history.sqlite3"),
-                  limit = 500,
-                },
+          "nvim-telescope/telescope.nvim",
+          opts = {
+            defaults = {
+              history = {
+                path = vim.fs.joinpath(vim.fn.stdpath("data"), "telescope_history.sqlite3"),
+                limit = 500,
               },
             },
           },
@@ -796,24 +793,22 @@ return {
       },
     },
     specs = {
-      {
-        "nvim-telescope/telescope.nvim",
-        opts = {
-          extensions = {
-            lazy = {
-              theme = "ivy",
-              show_icon = true,
-              mappings = {
-                open_in_browser = "<C-o>",
-                open_in_find_files = "<C-f>",
-                open_in_live_grep = "<C-g>",
-                open_in_file_browser = "<C-b>",
-                open_in_terminal = "<C-t>",
-                open_plugins_picker = "<C-o>", -- Works only after having called first another action
-                open_lazy_root_find_files = "<C-r>f",
-                open_lazy_root_live_grep = "<C-r>g",
-                change_cwd_to_plugin = "<C-r>d",
-              },
+      "nvim-telescope/telescope.nvim",
+      opts = {
+        extensions = {
+          lazy = {
+            theme = "ivy",
+            show_icon = true,
+            mappings = {
+              open_in_browser = "<C-o>",
+              open_in_find_files = "<C-f>",
+              open_in_live_grep = "<C-g>",
+              open_in_file_browser = "<C-b>",
+              open_in_terminal = "<C-t>",
+              open_plugins_picker = "<C-o>", -- Works only after having called first another action
+              open_lazy_root_find_files = "<C-r>f",
+              open_lazy_root_live_grep = "<C-r>g",
+              change_cwd_to_plugin = "<C-r>d",
             },
           },
         },
