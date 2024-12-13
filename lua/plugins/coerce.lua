766cc098616ecd8a321c2261c3733767ce23624b
Xavier Young <45989017+younger-1@users.noreply.github.com>
2024-12-13 22:40:20 +0800

    new: coerce
    Change-Id: Ic531f2f69dbcbc7d08e6941fdce4b38625dbd4ad

---
+++ lua/plugins/coerce.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "gregorias/coerce.nvim",
+    event = "LazyFile",
+    opts = {
+      default_mode_keymap_prefixes = {
+        normal_mode = "cy",
+        motion_mode = "gy",
+        visual_mode = "gy",
+      },
+    },
+  },
+}
