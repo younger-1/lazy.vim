09981297d9a9f9788719098a8e8e91915a346fb2
Xavier Young <45989017+younger-1@users.noreply.github.com>
2025-06-12 17:14:22 +0800

    vscode: basic keymap

--- lua/config/vscode.lua
+++ lua/config/vscode.lua
@@ -9,12 +9,25 @@ vim.api.nvim_create_autocmd("User", {
 
     map("", "<tab>", "%", { remap = true })
     map("", "<S-tab>", "g%", { remap = true })
-    map("n", "<BS>", "<C-^>")
+
+    -- ctrl-tab in all editor groups
+    map("n", "<BS>", "<cmd>call VSCodeNotify('workbench.action.quickOpenPreviousRecentlyUsedEditor')<cr>")
+    -- [edt mru]
+    map("n", "<S-BS>", "<cmd>call VSCodeNotify('workbench.action.showAllEditorsByMostRecentlyUsed')<cr>")
+
+    -- [edt]
+    map("n", "<leader>fb", "<cmd>call VSCodeNotify('workbench.action.showAllEditors')<cr>")
+    -- [edt active]
+    map("n", "<leader>fB", "<cmd>call VSCodeNotify('workbench.action.showEditorsInActiveGroup')<cr>")
 
     -- VSCode-specific keymaps for search and navigation
-    map("n", "<leader><space>", "<cmd>Find<cr>")
-    map("n", "<leader>/", [[<cmd>lua require('vscode').action('workbench.action.findInFiles')<cr>]])
-    map("n", "g<space>", [[<cmd>lua require('vscode').action('workbench.action.gotoSymbol')<cr>]])
+    -- map("n", "<leader><space>", "<cmd>Find<cr>")
+    map("n", "<leader><space>", "<cmd>call VSCodeNotify('workbench.action.quickOpen')<cr>")
+    -- map("n", "<leader>/", "<cmd>lua require('vscode').action('workbench.action.findInFiles')<cr>")
+    map("n", "<leader>/", "<cmd>call VSCodeNotify('workbench.action.findInFiles')<cr>")
+    map("n", "<leader>sg", "<Cmd>call VSCodeNotify('workbench.action.quickTextSearch')<CR>")
+
+    map("n", "g<space>", "<cmd>call VSCodeNotify('workbench.action.gotoSymbol')<cr>")
     map("n", "g<cr>", "<Cmd>call VSCodeNotify('breadcrumbs.focusAndSelect')<CR>")
 
     -- Keep undo/redo lists in sync with VsCode
